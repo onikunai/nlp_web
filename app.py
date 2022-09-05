0794ad0a4b533923c8cc43754e5c7b2f55491e78
onikunai <o66.okubo.shinya@gmail.com>
2022-09-05 11:28:58 +0900

    master

--- app.py
+++ app.py
@@ -10,29 +10,20 @@ app = Flask(__name__)
 def form():
     # POST処理
     if request.method == 'POST':
-        # print("POSTされたIDは？" + str(request.form['id']))
-        # print("POSTされたPASSWORDは？" + str(request.form['pwd']))
-        # return render_template('form.html')
+        # 自然言語処理で指定した品詞を抽出
         search_NLP = Search_NLP(str(request.form['search']))
-        print(search_NLP)
+        # print(search_NLP)
         
         # url = 'https://www.google.com/search?q=赤い+AND+丸い+AND+食べ物' # 確認用
         url = 'https://www.google.com/search?q=' + search_NLP
         
+        # ローカルのみ動作
         # ブラウザーOpen
         '''
         new・・・0(デフォルト):今までと同じウインドウで開く
         　　　　　1:新しいウインドウ、.open_new()メソッドと同じ動作
         　　　　　2:新しいタブ、.open_new_tab()メソッドと同じ動作
         '''
-        # if webbrowser.get('chrome'):
-        #     print('OK')
-        # else:
-        #     print('ng')
-        # webbro = webbrowser.get('chrome')
-        # webbro.open(url, new=2)
-        # webbrowser.get('chrome').open(url, new=2, autoraise=True)
-        # webbrowser.open_new_tab(url)
         # webbrowser.open(url, new=1)
         
         return render_template('jump.html', url=url)
@@ -42,9 +33,5 @@ def form():
 
 # アプリケーションを動かすためのおまじない
 if __name__ == "__main__":
-    # app.run(port = 8000, debug=True)
-    #port = os.Getenv("PORT")
-    #if port == "":
-    #    port = "8000"
-    #app.run(debug=True,  host='0.0.0.0', port=port) # ポートの変更
+    # port番号が取得できたら、その番号を使用。取得できなければ、5000とする。
     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
